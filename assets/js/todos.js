d1f59bb967b4b88c0dc9b6c4e725a13d11d7f0b8
nchchow <nelsonchow91.nc@gmail.com>
2020-03-29 16:47:57 -0700

    delete specific todos by clicking on X - functionality added

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,12 @@
 $("li").on("click", function() {
   $(this).toggleClass("done");
 });
+
+// delete specific todos by clicking on X
+$("span").on("click", function(event) {
+  // fade out li, then remove
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  });
+  event.stopPropagation();  // prevents event bubbling
+});
